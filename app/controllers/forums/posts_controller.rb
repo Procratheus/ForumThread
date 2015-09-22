a728696266d88b17349359563a55bee376616cfa
Alexander Lewis <alex.lewis79@yahoo.co.uk>
2015-09-23 00:59:07 +0100

    part 4

--- app/controllers/forums/posts_controller.rb
+++ app/controllers/forums/posts_controller.rb
@@ -1,2 +1,27 @@
 class Forums::PostsController < ApplicationController
+  before_action :authenticate_user!
+
+  def new
+    @forum = Forum.find(params[:forum_id])
+    @forum.posts.new
+  end
+
+  def create
+    @forum = Forum.find(params[:forum_id])
+    @post = @forum.posts.new(post_params)
+    @post.user_id = current_user.id
+
+    if @post.save
+      redirect_to forum_path(@forum, anchor: "post_#{@post.id}")
+    else
+      render :new
+    end
+  end
+
+  private
+
+   def post_params
+     params.require(:post).permit(:body)
+   end
+
 end
